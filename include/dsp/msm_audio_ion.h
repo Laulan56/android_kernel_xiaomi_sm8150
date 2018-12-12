db800280aa74a0e5d1578929875a464d30531381
Vikram Panduranga <vpandura@codeaurora.org>
2018-12-12 14:25:06 -0800

    dsp: add dma map resource cmd
    Add a new API for mapping DMA resources
    that needs to be attached to existing
    SMMU SID.
    
    Change-Id: I7a513d24ea30a5ead318e856e4c5317743fd0021
    Signed-off-by: Vikram Panduranga <vpandura@codeaurora.org>

--- include/dsp/msm_audio_ion.h
+++ include/dsp/msm_audio_ion.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, 2017-2018, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2015, 2017-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -16,6 +16,7 @@
 #include <dsp/q6asm-v2.h>
 #include <sound/pcm.h>
 #include <linux/msm_ion.h>
+#include <linux/dma-mapping.h>
 
 enum {
 	MSM_AUDIO_ION_INV_CACHES = 0,
@@ -34,4 +35,7 @@ int msm_audio_ion_cache_operations(struct audio_buffer *abuff, int cache_op);
 
 u32 msm_audio_populate_upper_32_bits(dma_addr_t pa);
 int msm_audio_ion_get_smmu_info(struct device **cb_dev, u64 *smmu_sid);
+
+int msm_audio_ion_dma_map(dma_addr_t *phys_addr, dma_addr_t *iova_base,
+			u32 size, enum dma_data_direction dir);
 #endif /* _LINUX_MSM_AUDIO_ION_H */
