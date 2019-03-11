b6802b2567d71074ace14241bd5cd161e560b8fb
Vatsal Bucha <vbucha@codeaurora.org>
2019-03-11 17:10:23 +0530

    soc: swr: Add wakeup source for swr device
    Voice call over headset does not disconnect on button press
    while device is in suspend state. This is because inband interrupt
    is not made wakeup capable. Make inband interrupt for swr tx as
    wakeup capable. Also add wakeup source for swr device.
    
    Change-Id: I5f2751672675bc8d62c4d2bfbbd651e640723d52
    Signed-off-by: Vatsal Bucha <vbucha@codeaurora.org>

--- soc/swr-mstr-ctrl.h
+++ soc/swr-mstr-ctrl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -170,6 +170,7 @@ struct swr_mstr_ctrl {
 	wait_queue_head_t pm_wq;
 	int wlock_holders;
 	u32 intr_mask;
+	u32 swr_irq_wakeup_capable;
 };
 
 #endif /* _SWR_WCD_CTRL_H */
