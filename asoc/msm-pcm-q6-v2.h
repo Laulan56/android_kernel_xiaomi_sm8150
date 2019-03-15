0a4cef07c68922f5a1f48eb21bf68ed2cc361132
Vignesh Kulothungan <vigneshk@codeaurora.org>
2019-03-15 11:25:15 -0700

    soc: pcm: add qtime mixer control
    Add qtime mixer control to receive qtime value in pcm driver.
    Add mechanism to delay asm using the time value received.
    Update platform data structure to hold pointers to all pcm's
    created in a platform.
    
    CRs-Fixed: 2419295
    Change-Id: I623e568196357af7909f3113d998df3c64501420
    Signed-off-by: Vignesh Kulothungan <vigneshk@codeaurora.org>

--- asoc/msm-pcm-q6-v2.h
+++ asoc/msm-pcm-q6-v2.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2008 Google, Inc.
  * Copyright (C) 2008 HTC Corporation
- * Copyright (c) 2012-2017 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2017,2019 The Linux Foundation. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -110,6 +110,8 @@ struct msm_audio {
 	bool meta_data_mode;
 	uint32_t volume;
 	bool compress_enable;
+	uint32_t qtime_lsw;
+	uint32_t qtime_msw;
 	/* array of frame info */
 	struct msm_audio_in_frame_info in_frame_info[CAPTURE_MAX_NUM_PERIODS];
 };
@@ -124,7 +126,7 @@ struct output_meta_data_st {
 
 struct msm_plat_data {
 	int perf_mode;
-	struct snd_pcm *pcm;
+	struct snd_pcm *pcm[MSM_FRONTEND_DAI_MAX];
 	struct msm_pcm_ch_map *ch_map[MSM_FRONTEND_DAI_MAX];
 };
 
