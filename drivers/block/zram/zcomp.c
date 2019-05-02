fcb456ecd44ab9feedc651abb473fce362bd9324
wloot <wlootlxt123@gmail.com>
2019-05-02 23:04:36 +0800

    zram: Set default compressor to lz4
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>
    Signed-off-by: zclkkk <2484392714@qq.com>

--- drivers/block/zram/zcomp.c
+++ drivers/block/zram/zcomp.c
@@ -19,9 +19,9 @@
 #include "zcomp.h"
 
 static const char * const backends[] = {
-	"lzo",
-#if IS_ENABLED(CONFIG_CRYPTO_LZ4)
 	"lz4",
+#if IS_ENABLED(CONFIG_CRYPTO_LZO)
+	"lzo",
 #endif
 #if IS_ENABLED(CONFIG_CRYPTO_DEFLATE)
 	"deflate",
