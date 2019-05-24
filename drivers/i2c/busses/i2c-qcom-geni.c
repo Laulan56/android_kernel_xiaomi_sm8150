3d69256cc756e7f5e6f12529b7432a80ccf04af6
Rick Yiu <rickyiu@google.com>
2019-05-24 14:40:07 +0800

    i2c-qcom-geni: move geni_i2c_driver_init to async probe
    To reduce boot time to first stage.
    
    Before:
    [    1.221918] init: init first stage started!
    
    After:
    [    1.182935] init: init first stage started!
    
    Bug: 129688998
    Test: reboot 100 times, camera, wifi, basic operation
    Change-Id: I25aaf2bd7e1a031fa85930ddfb570c86b8c8ce79
    Signed-off-by: Rick Yiu <rickyiu@google.com>

--- drivers/i2c/busses/i2c-qcom-geni.c
+++ drivers/i2c/busses/i2c-qcom-geni.c
@@ -1293,6 +1293,7 @@ static struct platform_driver geni_i2c_driver = {
 		.name = "i2c_geni",
 		.pm = &geni_i2c_pm_ops,
 		.of_match_table = geni_i2c_dt_match,
+		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 };
 
