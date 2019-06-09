d2923a812148ecbb41d75214271ba6a610618114
Demon000 <demonsingur@gmail.com>
2019-06-09 19:38:12 +0200

    scsi: ufs: add WAIT_AFTER_REF_CLK_UNGATE quirk to Sandisk devices
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

--- drivers/scsi/ufs/ufs_quirks.h
+++ drivers/scsi/ufs/ufs_quirks.h
@@ -26,6 +26,7 @@
 #define UFS_VENDOR_SAMSUNG     0x1CE
 #define UFS_VENDOR_SKHYNIX     0x1AD
 #define UFS_VENDOR_WDC		0x145
+#define UFS_VENDOR_SANDISK     0x0145
 
 /**
  * ufs_dev_fix - ufs device quirk info
