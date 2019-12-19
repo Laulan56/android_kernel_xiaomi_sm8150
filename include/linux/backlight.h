901adb9c5d68ca2df2e648805b5d36b7e87cce5a
Demon000 <demonsingur@gmail.com>
2019-12-19 13:25:53 +0100

    brightness: trigger backlight update events
    Change-Id: Ic9fcd95d8417ce5095443fb338944006453795b7
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- include/linux/backlight.h
+++ include/linux/backlight.h
@@ -45,6 +45,12 @@ enum backlight_type {
 enum backlight_notification {
 	BACKLIGHT_REGISTERED,
 	BACKLIGHT_UNREGISTERED,
+	BACKLIGHT_UPDATED,
+};
+
+enum backlight_brightness {
+	BACKLIGHT_OFF,
+	BACKLIGHT_ON,
 };
 
 struct backlight_device;
