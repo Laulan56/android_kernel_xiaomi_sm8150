f35a43ec56109db6647f60d71fcfcbe876be3e99
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-07 23:36:58 -0800

    simple_lmk: Disable OOM killer when Simple LMK is enabled
    The OOM killer only serves to be a liability when Simple LMK is used.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -1018,7 +1018,7 @@ bool out_of_memory(struct oom_control *oc)
 	unsigned long freed = 0;
 	enum oom_constraint constraint = CONSTRAINT_NONE;
 
-	if (oom_killer_disabled)
+	if (oom_killer_disabled || IS_ENABLED(CONFIG_ANDROID_SIMPLE_LMK))
 		return false;
 
 	if (try_online_one_block(numa_node_id())) {
