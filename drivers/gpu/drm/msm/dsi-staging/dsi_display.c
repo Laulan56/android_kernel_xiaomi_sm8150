b7ef9b382952942f8176e3d7a60214c3c7bf47e3
Vara Reddy <varar@codeaurora.org>
2020-06-12 20:19:12 +0800

    drm/msm/dsi-staging: update frame transfer time calculations
    Change updates frame transfer time calculations. Frame threshold
    is provided as input to decide on the final transfer time.
    Panel dsi clock node followed by mdp transfer time node
    will take priority in selecting final transfer time than frame
    threshold time.
    
    Change-Id: I40c3abfc635cd9b338b705535612ac32e047ce6e
    Signed-off-by: Vara Reddy <varar@codeaurora.org>
    Signed-off-by: Ray Zhang <rayz@codeaurora.org>
    Signed-off-by: Bruce Hoo <bingchua@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -6577,6 +6577,7 @@ int dsi_display_get_modes(struct dsi_display *display,
 			  struct dsi_display_mode **out_modes)
 {
 	struct dsi_dfps_capabilities dfps_caps;
+	struct dsi_display_ctrl *ctrl;
 	struct dsi_host_common_cfg *host = &display->panel->host_config;
 	bool is_split_link, is_cmd_mode;
 	u32 num_dfps_rates, timing_mode_count, display_mode_count;
@@ -6590,6 +6591,7 @@ int dsi_display_get_modes(struct dsi_display *display,
 	}
 
 	*out_modes = NULL;
+	ctrl = &display->ctrl[0];
 
 	mutex_lock(&display->display_lock);
 
@@ -6619,6 +6621,7 @@ int dsi_display_get_modes(struct dsi_display *display,
 	for (mode_idx = 0; mode_idx < timing_mode_count; mode_idx++) {
 		struct dsi_display_mode display_mode;
 		int topology_override = NO_OVERRIDE;
+		u32 frame_threshold_us = ctrl->ctrl->frame_threshold_time_us;
 
 		if (display->cmdline_timing == mode_idx)
 			topology_override = display->cmdline_topology;
@@ -6643,7 +6646,7 @@ int dsi_display_get_modes(struct dsi_display *display,
 		if (is_cmd_mode) {
 			dsi_panel_calc_dsi_transfer_time(
 				&display->panel->host_config,
-				&display_mode.timing);
+				&display_mode, frame_threshold_us);
 			display_mode.priv_info->dsi_transfer_time_us =
 				display_mode.timing.dsi_transfer_time_us;
 			display_mode.priv_info->min_dsi_clk_hz =
