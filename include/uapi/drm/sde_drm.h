e614bb74b1435b5e92b94a1595b4dbc0a759a7fa
Pig <pig.priv@gmail.com>
2020-08-14 20:06:43 +0800

    drm: msm: Export FOD_PRESSED_LAYER_ZORDER to uapi
    Change-Id: I5b25d19fab3a2901948d639c785165e70786f4ab

--- include/uapi/drm/sde_drm.h
+++ include/uapi/drm/sde_drm.h
@@ -495,4 +495,9 @@ struct sde_drm_roi_misr_v1 {
 #define SDE_RECOVERY_CAPTURE		1
 #define SDE_RECOVERY_HARD_RESET		2
 
+/**
+ * sde fod dim layer
+ */
+#define FOD_PRESSED_LAYER_ZORDER 0x20000000u
+
 #endif /* _SDE_DRM_H_ */
