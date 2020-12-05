8c85151700e70fa5dbb37edfba616aa5e9a24e0d
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-12-05 12:05:03 +0530

    nfc: nq-nci: Disable NFC for Indian devices
    Change-Id: Iadb9c4978799e347dad1fec80de05174170b6d64
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/nfc/nq-nci.c
+++ drivers/nfc/nq-nci.c
@@ -85,6 +85,8 @@ struct nqx_dev {
 	struct nqx_platform_data *pdata;
 };
 
+extern char *saved_command_line;
+
 static int nfcc_reboot(struct notifier_block *notifier, unsigned long val,
 			void *v);
 /*clock enable function*/
@@ -1230,6 +1232,13 @@ static int nqx_probe(struct i2c_client *client,
 	struct nqx_dev *nqx_dev;
 
 	dev_dbg(&client->dev, "%s: enter\n", __func__);
+
+	if (strnstr(saved_command_line, "androidboot.hwc=India",
+					strlen(saved_command_line)) != NULL) {
+		dev_err(&client->dev, "%s:NFC HWC India : Do not probe nqx\n", __func__);
+		return -ENODEV;
+	}
+
 	if (client->dev.of_node) {
 		platform_data = devm_kzalloc(&client->dev,
 			sizeof(struct nqx_platform_data), GFP_KERNEL);
