66da1d1fce95df25d1791925529b61017656eeff
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-24 22:53:54 -0800

    msm: ipa: Refactor buffer size check in ipa3_write()
    This confuses GCC's FORTIFY_SOURCE check for the copy_from_user() in
    this function. Simplify it to resolve the GCC warning.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I963ae2b81e4ac7cb46c08ece73bb7cd1201a621e

--- drivers/platform/msm/ipa/ipa_v3/ipa.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa.c
@@ -6690,7 +6690,7 @@ static ssize_t ipa3_write(struct file *file, const char __user *buf,
 
 	int i = 0;
 
-	if (sizeof(dbg_buff) < count + 1)
+	if (count >= sizeof(dbg_buff))
 		return -EFAULT;
 
 	missing = copy_from_user(dbg_buff, buf, min(sizeof(dbg_buff), count));
