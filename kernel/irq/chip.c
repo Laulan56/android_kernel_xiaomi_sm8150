04bbf7406c79cabf4e456d38e4d3d01e3bb94733
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-09 18:01:49 -0800

    kernel: Add tri-cluster API to affine IRQs and kthreads to fast CPUs
    On devices with a CPU that contains heterogeneous cores (e.g.,
    big.LITTLE), it can be beneficial to place some performance-critical
    IRQs and kthreads onto the fast CPU clusters in order to improve
    performance.
    
    This commit adds the following APIs:
    -kthread_run_perf_critical() to create and start a perf-critical kthread
     and affine it to the given performance CPUs
    -irq_set_perf_affinity() to mark an active IRQ as perf-critical and
     affine it to the given performance CPUs
    -IRQF_PERF_AFFINE and IRQF_PRIME_AFFINE to schedule an IRQ and any
     threads it may have onto the given performance CPUs
    -PF_PERF_CRITICAL to mark a process (mainly a kthread) as performance
     critical (this is used by kthread_run_perf_critical())
    
    In order to accommodate this new API, the following changes are made:
    -Performance-critical IRQs are distributed evenly among online CPUs
     available in the given performance CPU mask
    -Performance-critical IRQs have their affinities reaffined upon exit
     from suspend (since the affinities are broken when non-boot CPUs are
     disabled)
    -Performance-critical IRQs and their threads have their affinities reset
     upon entering suspend, so that upon immediate suspend exit (when only
     the boot CPU is online), interrupts can be processed and interrupt
     threads can be scheduled onto an online CPU (otherwise we'd hit a
     kernel BUG)
    -__set_cpus_allowed_ptr() is modified to enforce a performance-critical
     kthread's affinity
    -Perf-critical IRQs aren't allowed to have their affinity changed by
     userspace
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I71258b73f113ec087239e544d381c54fba979b85
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -263,7 +263,10 @@ int irq_startup(struct irq_desc *desc, bool resend, bool force)
 		switch (__irq_startup_managed(desc, aff, force)) {
 		case IRQ_STARTUP_NORMAL:
 			ret = __irq_startup(desc);
-			irq_setup_affinity(desc);
+			if (irqd_has_set(&desc->irq_data, IRQD_PERF_CRITICAL))
+				setup_perf_irq_locked(desc, desc->action->flags);
+			else
+				irq_setup_affinity(desc);
 			break;
 		case IRQ_STARTUP_MANAGED:
 			irq_do_set_affinity(d, aff, false);
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1120,6 +1120,11 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	int ret = 0;
 	cpumask_t allowed_mask;
 
+	/* Don't allow perf-critical threads to have non-perf affinities */
+	if ((p->flags & PF_PERF_CRITICAL) && new_mask != cpu_perf_mask &&
+	    new_mask != cpu_prime_mask)
+		return -EINVAL;
+
 	rq = task_rq_lock(p, &rf);
 	update_rq_clock(rq);
 
