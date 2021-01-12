6540e0e416e9ddb798b3a46e738efef743d076f9
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-11 23:35:25 -0800

    arm64: arch_timer: Fix unused function warnings when OOL_WORKAROUND=n
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: Ie6f0487fa8628ff18a06183d5c05e28c8a96cdb4

--- drivers/clocksource/arm_arch_timer.c
+++ drivers/clocksource/arm_arch_timer.c
@@ -290,6 +290,7 @@ static struct ate_acpi_oem_info hisi_161010101_oem_info[] = {
 };
 #endif
 
+#ifdef CONFIG_ARM_ARCH_TIMER_OOL_WORKAROUND
 #ifdef CONFIG_ARM64_ERRATUM_858921
 static u64 notrace arm64_858921_read_cntvct_el0(void)
 {
@@ -308,7 +309,6 @@ static u64 notrace arm64_1188873_read_cntvct_el0(void)
 }
 #endif
 
-#ifdef CONFIG_ARM_ARCH_TIMER_OOL_WORKAROUND
 DEFINE_PER_CPU(const struct arch_timer_erratum_workaround *,
 	       timer_unstable_counter_workaround);
 EXPORT_SYMBOL_GPL(timer_unstable_counter_workaround);
