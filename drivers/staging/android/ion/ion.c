cb64bb74400b1aa1655425d8eae22c69a83e0b4d
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-28 17:07:11 -0800

    ion: Mark workqueues freeing buffers asynchronously as CPU intensive
    When exiting the camera, there's a period of intense lag caused by all
    of the buffer-free workers consuming all CPUs at once for a few seconds.
    This isn't very good, and freeing the buffers isn't super time critical,
    so we can lower the burden of the workers by marking the per-heap
    workqueues as CPU intensive, which offloads the burden of balancing the
    workers onto the scheduler.
    
    Also, mark these workqueues with WQ_MEM_RECLAIM so forward progress is
    guaranteed via a rescuer thread, since these are used to free memory.
    The unnecessary WQ_UNBOUND_MAX_ACTIVE is removed as well, since it's
    only used for increasing the active worker count on large-CPU systems.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -547,8 +547,8 @@ static int ion_alloc_fd(struct ion_allocation_data *a)
 void ion_device_add_heap(struct ion_device *idev, struct ion_heap *heap)
 {
 	if (heap->flags & ION_HEAP_FLAG_DEFER_FREE) {
-		heap->wq = alloc_workqueue("%s", WQ_UNBOUND,
-					   WQ_UNBOUND_MAX_ACTIVE, heap->name);
+		heap->wq = alloc_workqueue("%s", WQ_UNBOUND | WQ_MEM_RECLAIM |
+					   WQ_CPU_INTENSIVE, 0, heap->name);
 		BUG_ON(!heap->wq);
 	}
 
