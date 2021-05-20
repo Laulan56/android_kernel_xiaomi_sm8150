497e6554afcee91bef2b17a0396af49b6d96013c
Kunlei Zhang <kunleiz@codeaurora.org>
2021-05-20 16:02:30 +0800

    dsp: update size check for set/get VI param function
    In afe_set_cal_sp_th_vi_cfg and afe_get_cal_sp_th_vi_param
    functions, data size is not checked properly which might
    lead to out of bounds access.
    
    Add data_size check to avoid out of bounds access.
    
    Change-Id: Ibe049300c609d0d717309bccc65cac876ac075f7
    Signed-off-by: Kunlei Zhang <kunleiz@codeaurora.org>

--- dsp/q6afe.c
+++ dsp/q6afe.c
@@ -7987,6 +7987,9 @@ static int afe_set_cal_sp_th_vi_cfg(int32_t cal_type, size_t data_size,
 
 	if (cal_data == NULL ||
 	    data_size > sizeof(*cal_data) ||
+	    (data_size < sizeof(cal_data->cal_hdr) +
+		sizeof(cal_data->cal_data) +
+		sizeof(cal_data->cal_info.mode)) ||
 	    this_afe.cal_data[AFE_FB_SPKR_PROT_TH_VI_CAL] == NULL)
 		goto done;
 
@@ -8131,7 +8134,9 @@ static int afe_get_cal_sp_th_vi_param(int32_t cal_type, size_t data_size,
 
 	if (cal_data == NULL ||
 	    data_size > sizeof(*cal_data) ||
-	    data_size < sizeof(cal_data->cal_hdr) ||
+	    (data_size < sizeof(cal_data->cal_hdr) +
+		sizeof(cal_data->cal_data) +
+		sizeof(cal_data->cal_info.mode)) ||
 	    this_afe.cal_data[AFE_FB_SPKR_PROT_TH_VI_CAL] == NULL)
 		return 0;
 
