ffbeb412af59d0f7604f14cea699ee20d63b5f1c
Aditya Bavanari <abavanar@codeaurora.org>
2021-06-11 12:36:23 +0530

    asoc: Set and get voice UI port indices on a per session basis
    When multiple voice UI sessions are running, voice UI port
    index might be overwritten with index of different sessions
    during concurrent LPI and Non LPI session switches. Maintain
    voice UI port index on a per session basis to avoid this issue.
    
    Change-Id: I6b47c562e36795aff37a42fe10ae0c7c660bb82b
    Signed-off-by: Aditya Bavanari <abavanar@codeaurora.org>

--- include/dsp/q6lsm.h
+++ include/dsp/q6lsm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2019, 2021, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -27,6 +27,8 @@
 #define LSM_V3P0_MAX_NUM_CHANNELS 9
 
 #define LSM_API_VERSION_V3 3
+
+#define MAX_LSM_SESSIONS 8
 
 typedef void (*lsm_app_cb)(uint32_t opcode, uint32_t token,
 		       uint32_t *payload, uint16_t client_size, void *priv);
