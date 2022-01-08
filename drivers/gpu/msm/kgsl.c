5d6667b4c75cc35e48745d759153dabd8ba1bdcb
Stephen Xian <stevexian56@gmail.com>
2022-01-08 17:21:10 +0800

    msm: kgsl: Affine IRQ and worker kthread to the little CPU cluster
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5239,7 +5239,7 @@ static int __init kgsl_core_init(void)
 
 	kthread_init_worker(&kgsl_driver.worker);
 
-	kgsl_driver.worker_thread = kthread_run(kthread_worker_fn,
+	kgsl_driver.worker_thread = kthread_run_perf_critical(cpu_lp_mask, kthread_worker_fn,
 		&kgsl_driver.worker, "kgsl_worker_thread");
 
 	if (IS_ERR(kgsl_driver.worker_thread)) {
