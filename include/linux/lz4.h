3a918fc5caede8fe943fa2cfa9afbeb1901802dd
Stephen Xian <stevexian56@gmail.com>
2022-01-08 16:22:19 +0800

    include/linux: lz4: Increase LZ4 memory usage to 64KB
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -55,7 +55,7 @@
  * Reduced memory usage can improve speed, due to cache effect
  * Default value is 14, for 16KB, which nicely fits into Intel x86 L1 cache
  */
-#define LZ4_MEMORY_USAGE 14
+#define LZ4_MEMORY_USAGE 16
 
 #define LZ4_MAX_INPUT_SIZE	0x7E000000 /* 2 113 929 216 bytes */
 #define LZ4_COMPRESSBOUND(isize)	(\
