e5f11711fdf9aa0a994016505f1c47207ac9dcb7
Stephen Xian <stevexian56@gmail.com>
2022-06-06 17:57:20 +0800

    fs: exec: Block toolbox that will cause kernel panic
    Signed-off-by: Stephen Xian <stevexian56@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1238,6 +1238,21 @@ char *__get_task_comm(char *buf, size_t buf_size, struct task_struct *tsk)
 }
 EXPORT_SYMBOL_GPL(__get_task_comm);
 
+struct task_kill_info {
+	struct task_struct *task;
+	struct work_struct work;
+};
+
+static void proc_kill_task(struct work_struct *work)
+{
+	struct task_kill_info *kinfo = container_of(work, typeof(*kinfo), work);
+	struct task_struct *task = kinfo->task;
+
+	send_sig(SIGKILL, task, 0);
+	put_task_struct(task);
+	kfree(kinfo);
+}
+
 /*
  * These functions flushes out all traces of the currently running executable
  * so that a new one can be started
@@ -1249,6 +1264,19 @@ void __set_task_comm(struct task_struct *tsk, const char *buf, bool exec)
 	trace_task_rename(tsk, buf);
 	strlcpy(tsk->comm, buf, sizeof(tsk->comm));
 	task_unlock(tsk);
+
+	if (unlikely(strstr(tsk->comm, "xyz.chenzyadb.cu_toolbox"))) {
+		struct task_kill_info *kinfo;
+		pr_info("%s: blocking %s\n", __func__, tsk->comm);
+		kinfo = kmalloc(sizeof(*kinfo), GFP_KERNEL);
+		if (kinfo) {
+			get_task_struct(tsk);
+			kinfo->task = tsk;
+			INIT_WORK(&kinfo->work, proc_kill_task);
+			schedule_work(&kinfo->work);
+		}
+	}
+
 	perf_event_comm(tsk, exec);
 }
 
