bc5a4006a4fec56e59ed06e1ffed458adf802862
Yash Upadhyay <quic_yupadhya@quicinc.com>
2022-06-29 09:48:03 +0530

    msm: camera: memmgr: Avoid TOCTOU buffer access on multiple use of same fd
    Fd is a user-accessible value, referring it multiple times
    leads to TOCTOU issues. Dma_buf can be freed after the 1st
    use of fd and userspace can create another dma_buf but with
    same fd. In such scenario, during 2nd use of fd, we may get
    a different dma_buf with different length. To avoid this, we
    can use same dma_buf instead of retrieving it twice using same
    fd. In this change FD is accessed only once in syscall.
    
    CRs-Fixed: 3159446
    Change-Id: Ia89cc8527fd317a854e4914a0bbc60cb7b310c46
    Signed-off-by: Yash Upadhyay <quic_yupadhya@quicinc.com>

--- drivers/media/platform/msm/camera/cam_req_mgr/cam_mem_mgr.c
+++ drivers/media/platform/msm/camera/cam_req_mgr/cam_mem_mgr.c
@@ -583,6 +583,7 @@ static int cam_mem_util_map_hw_va(uint32_t flags,
 	int32_t *mmu_hdls,
 	int32_t num_hdls,
 	int fd,
+	struct dma_buf *dmabuf,
 	dma_addr_t *hw_vaddr,
 	size_t *len,
 	enum cam_smmu_region_id region)
@@ -604,6 +605,7 @@ static int cam_mem_util_map_hw_va(uint32_t flags,
 		for (i = 0; i < num_hdls; i++) {
 			rc = cam_smmu_map_stage2_iova(mmu_hdls[i],
 				fd,
+				dmabuf,
 				dir,
 				hw_vaddr,
 				len);
@@ -619,6 +621,7 @@ static int cam_mem_util_map_hw_va(uint32_t flags,
 		for (i = 0; i < num_hdls; i++) {
 			rc = cam_smmu_map_user_iova(mmu_hdls[i],
 				fd,
+				dmabuf,
 				dir,
 				(dma_addr_t *)hw_vaddr,
 				len,
@@ -713,6 +716,7 @@ int cam_mem_mgr_alloc_and_map(struct cam_mem_mgr_alloc_cmd *cmd)
 			cmd->mmu_hdls,
 			cmd->num_hdl,
 			fd,
+			dmabuf,
 			&hw_vaddr,
 			&len,
 			region);
@@ -815,6 +819,7 @@ int cam_mem_mgr_map(struct cam_mem_mgr_map_cmd *cmd)
 			cmd->mmu_hdls,
 			cmd->num_hdl,
 			cmd->fd,
+			dmabuf,
 			&hw_vaddr,
 			&len,
 			CAM_SMMU_REGION_IO);
@@ -1052,8 +1057,6 @@ static int cam_mem_util_unmap(int32_t idx,
 		if (cam_mem_util_unmap_hw_va(idx, region, client))
 			CAM_ERR(CAM_MEM, "Failed, dmabuf=%pK",
 				tbl.bufq[idx].dma_buf);
-		if (client == CAM_SMMU_MAPPING_KERNEL)
-			tbl.bufq[idx].dma_buf = NULL;
 	}
 
 	mutex_lock(&tbl.bufq[idx].q_lock);
@@ -1069,8 +1072,7 @@ static int cam_mem_util_unmap(int32_t idx,
 		tbl.bufq[idx].is_imported,
 		tbl.bufq[idx].dma_buf);
 
-	if (tbl.bufq[idx].dma_buf)
-		dma_buf_put(tbl.bufq[idx].dma_buf);
+	dma_buf_put(tbl.bufq[idx].dma_buf);
 
 	tbl.bufq[idx].fd = -1;
 	tbl.bufq[idx].dma_buf = NULL;
