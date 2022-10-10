c1786feb7f116a3c54abf7c7e2ad4139b3a4efa2
Shubham Talekar <quic_stalekar@quicinc.com>
2022-10-10 17:06:55 +0530

    drm/msm/dp: remove dp post_open call as callback notifier
    Remove DP post open call during successive HPD handling
    as part of notifier callback. DP callback notifier is already
    notified via connect work queue therefore there is no need
    to initialize post_open callback. Also, skip HDCP and Audio
    enablement over DP during early bootsplash screen.
    Anyways as part of normal boot sequence, HDCP and Audio
    are enabled over DP.
    
    Change-Id: I1e53024efd2c3f1c03d6b02d643b33d7cd7891c5
    Signed-off-by: Shubham Talekar <quic_stalekar@quicinc.com>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -128,14 +128,16 @@ static const struct of_device_id dp_dt_match[] = {
 };
 
 static void dp_display_update_hdcp_info(struct dp_display_private *dp);
-static bool dp_display_framework_ready(struct dp_display_private *dp)
-{
-	return dp->dp_display.post_open ? false : true;
-}
 
 static inline bool dp_display_is_hdcp_enabled(struct dp_display_private *dp)
 {
 	return dp->link->hdcp_status.hdcp_version && dp->hdcp.ops;
+}
+
+static bool is_drm_bootsplash_enabled(struct device *dev)
+{
+	return of_property_read_bool(dev->of_node,
+		"qcom,sde-drm-fb-splash-logo-enabled");
 }
 
 static irqreturn_t dp_display_irq(int irq, void *dev_id)
@@ -348,13 +350,6 @@ static void dp_display_hdcp_cb_work(struct work_struct *work)
 	u8 sink_status = 0;
 
 	dp = container_of(dw, struct dp_display_private, hdcp_cb_work);
-
-	dp_display_update_hdcp_info(dp);
-
-	if (!dp_display_is_hdcp_enabled(dp))
-		return;
-
-	dp->link->hdcp_status.hdcp_state = HDCP_STATE_AUTHENTICATING;
 
 	if (!dp->power_on || !dp->is_connected || atomic_read(&dp->aborted) ||
 			dp->hdcp_abort)
@@ -641,40 +636,11 @@ static void dp_display_send_hpd_event(struct dp_display_private *dp)
 			envp);
 }
 
-static void dp_display_post_open(struct dp_display *dp_display)
-{
-	struct drm_connector *connector;
-	struct dp_display_private *dp;
-
-	if (!dp_display) {
-		pr_err("invalid input\n");
-		return;
-	}
-
-	dp = container_of(dp_display, struct dp_display_private, dp_display);
-	if (IS_ERR_OR_NULL(dp)) {
-		pr_err("invalid params\n");
-		return;
-	}
-
-	connector = dp->dp_display.base_connector;
-
-	if (!connector) {
-		pr_err("connector not set\n");
-		return;
-	}
-
-	/* if cable is already connected, send notification */
-	if (dp->hpd->hpd_high)
-		queue_work(dp->wq, &dp->connect_work);
-	else
-		dp_display->post_open = NULL;
-}
-
 static int dp_display_send_hpd_notification(struct dp_display_private *dp)
 {
 	int ret = 0;
 	bool hpd = dp->is_connected;
+	static int bootsplash_count;
 
 	dp->aux->state |= DP_STATE_NOTIFICATION_SENT;
 
@@ -683,13 +649,12 @@ static int dp_display_send_hpd_notification(struct dp_display_private *dp)
 	else
 		dp->dp_display.is_sst_connected = false;
 
-	if (!dp_display_framework_ready(dp)) {
-		pr_debug("%s: dp display framework not ready\n", __func__);
-		if (!dp->dp_display.is_bootsplash_en) {
-			dp->dp_display.is_bootsplash_en = true;
-			drm_client_dev_register(dp->dp_display.drm_dev);
-		}
-		return ret;
+	if (!dp->dp_display.is_bootsplash_en
+		&& is_drm_bootsplash_enabled(dp->dp_display.drm_dev->dev)
+		&& !bootsplash_count) {
+		dp->dp_display.is_bootsplash_en = true;
+		bootsplash_count++;
+		drm_client_dev_register(dp->dp_display.drm_dev);
 	}
 
 	reinit_completion(&dp->notification_comp);
@@ -1258,11 +1223,6 @@ static void dp_display_connect_work(struct work_struct *work)
 	struct dp_display_private *dp = container_of(work,
 			struct dp_display_private, connect_work);
 
-	if (dp->dp_display.is_sst_connected && dp_display_framework_ready(dp)) {
-		pr_debug("HPD already on\n");
-		return;
-	}
-
 	if (atomic_read(&dp->aborted)) {
 		pr_warn("HPD off requested\n");
 		return;
@@ -1761,6 +1721,11 @@ static int dp_display_post_enable(struct dp_display *dp_display, void *panel)
 	dp_panel = panel;
 
 	mutex_lock(&dp->session_lock);
+
+	if (dp->dp_display.is_bootsplash_en) {
+		dp->dp_display.is_bootsplash_en = false;
+		goto end;
+	}
 
 	if (!dp->power_on) {
 		pr_debug("stream not setup, return\n");
@@ -2841,7 +2806,7 @@ static int dp_display_probe(struct platform_device *pdev)
 	g_dp_display->unprepare     = dp_display_unprepare;
 	g_dp_display->request_irq   = dp_request_irq;
 	g_dp_display->get_debug     = dp_get_debug;
-	g_dp_display->post_open     = dp_display_post_open;
+	g_dp_display->post_open     = NULL;
 	g_dp_display->post_init     = dp_display_post_init;
 	g_dp_display->config_hdr    = dp_display_config_hdr;
 	g_dp_display->mst_install   = dp_display_mst_install;
