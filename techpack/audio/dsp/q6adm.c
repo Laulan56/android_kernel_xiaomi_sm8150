44b6d1bd313b823eae3fd86530aadd4162f5de37
Soumya Managoli <quic_c_smanag@quicinc.com>
2023-04-05 15:46:55 +0530

    dsp: q6adm: Resolve mem corruption in adm cb
    Conversion of negative "num_modules" var value
    will result in max possible unsigned int value
    and hence can cause mem corruption when accessed.
    
    Resolve this by assigning the same data type
    to "num_modules" var as used in the calling fn.
    
    Change-Id: I4c9d7215b9c7345637e1eb3a1992a41fef71c5cb
    Signed-off-by: Soumya Managoli <quic_c_smanag@quicinc.com>

--- techpack/audio/dsp/q6adm.c
+++ techpack/audio/dsp/q6adm.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2012-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1562,7 +1563,7 @@ static int32_t adm_callback(struct apr_client_data *data, void *priv)
 {
 	uint32_t *payload;
 	int port_idx, copp_idx, idx, client_id;
-	int num_modules;
+	uint32_t num_modules;
 	int ret;
 
 	if (data == NULL) {
