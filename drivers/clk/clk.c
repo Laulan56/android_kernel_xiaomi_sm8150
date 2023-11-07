5fe8afd214b5f4e1e465663f4f24fdcf9aab5f71
basamaryan <basam.aryan@gmail.com>
2023-11-07 05:04:41 +0100

    clk: Fix mismerge
    The all_lists structure was placed in the wrong location, causing
    the build to fail when CONFIG_DEBUG_FS is not defined.
    
    Fix this up by moving the struct to the correct place in the file.
    
    Change-Id: I18c5ed3491845583d80dcf1a7bfde41eaf904f98

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -45,6 +45,12 @@ static HLIST_HEAD(clk_root_list);
 static HLIST_HEAD(clk_orphan_list);
 static LIST_HEAD(clk_notifier_list);
 
+static struct hlist_head *all_lists[] = {
+	&clk_root_list,
+	&clk_orphan_list,
+	NULL,
+};
+
 struct clk_handoff_vdd {
 	struct list_head list;
 	struct clk_vdd_class *vdd_class;
@@ -2692,12 +2698,6 @@ static int inited = 0;
 static u32 debug_suspend;
 static DEFINE_MUTEX(clk_debug_lock);
 static HLIST_HEAD(clk_debug_list);
-
-static struct hlist_head *all_lists[] = {
-	&clk_root_list,
-	&clk_orphan_list,
-	NULL,
-};
 
 static struct hlist_head *orphan_list[] = {
 	&clk_orphan_list,
